4735e08876f0cc0bf3324ac135d44d7153c06248
KonDzioUrbanik <konradsparta@o2.pl>
2026-03-13 10:37:38 +0100

    poprawka

--- src/main/java/com/pansgroup/projectbackend/controller/AdminController.java
+++ src/main/java/com/pansgroup/projectbackend/controller/AdminController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/admin") // Wszystkie metody bbeda zaczynac sis od /admin
-@PreAuthorize("hasRole('ADMIN')") // Zabezpieczenie: Tylko admin wejdzie do tej klasy
+@PreAuthorize("hasRole('ADMIN')") // Zabezpieczenie: Tylko admin wejdzie do tej klasy tak
 public class AdminController{
 
     private final UserService userService;
